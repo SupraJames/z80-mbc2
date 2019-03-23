181a451e262c82395741bff0f1fddb1053b2cb55
James Pattinson <james@pattinson.org>
2019-03-23 17:28:17 +0000

    IOCON.BANK = 1

--- S220718-R190918_IOS-Z80-MBC2-KP/S220718-R190918_IOS-Z80-MBC2-KP.ino
+++ S220718-R190918_IOS-Z80-MBC2-KP/S220718-R190918_IOS-Z80-MBC2-KP.ino
@@ -107,11 +107,11 @@ S220718-R190918   Added support for CP/M 3.
 
 #define   GPIOEXP_ADDR  0x20  // I2C module address (see datasheet)
 #define   IODIRA_REG    0x00  // MCP23017 internal register IODIRA  (see datasheet)
-#define   IODIRB_REG    0x01  // MCP23017 internal register IODIRB  (see datasheet)
-#define   GPPUA_REG     0x0C  // MCP23017 internal register GPPUA  (see datasheet)
-#define   GPPUB_REG     0x0D  // MCP23017 internal register GPPUB  (see datasheet)
-#define   GPIOA_REG     0x12  // MCP23017 internal register GPIOA  (see datasheet)
-#define   GPIOB_REG     0x13  // MCP23017 internal register GPIOB  (see datasheet)
+#define   IODIRB_REG    0x10  // MCP23017 internal register IODIRB  (see datasheet)
+#define   GPPUA_REG     0x06  // MCP23017 internal register GPPUA  (see datasheet)
+#define   GPPUB_REG     0x16  // MCP23017 internal register GPPUB  (see datasheet)
+#define   GPIOA_REG     0x09  // MCP23017 internal register GPIOA  (see datasheet)
+#define   GPIOB_REG     0x19  // MCP23017 internal register GPIOB  (see datasheet)
 
 // ------------------------------------------------------------------------------
 //
